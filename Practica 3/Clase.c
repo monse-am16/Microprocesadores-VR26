284eaf8f39b806262d7f5b13251fd5b172c3ed28
Diego Castro <diego.castro@udem.edu>
2026-06-07 19:42:33 -0600

    Add Clase.c file

---
+++ Practica 3/Clase.c
@@ -0,0 +1,36 @@
+#include <xc.h>         // Biblioteca principal del compilador XC8
+
+//=============================================================================
+// CONFIGURACI N DE BITS DE CONFIGURACI N (FUSES)
+//=============================================================================
+
+// Selecci n de oscilador (usar XT si est s usando un cristal de 4 MHz)
+#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (enabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (disabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection (disabled)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable (disabled)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (disabled)
+
+//=============================================================================
+// DEFINICIONES
+//=============================================================================
+
+#define _XTAL_FREQ 8000000      // Frecuencia del oscilador (para __delay_ms y __delay_us)
+
+unsigned char display[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+
+void main(void){
+    
+    TRISD = 0; 
+    PORTD = 0; // Establecer puerto D como salida. 
+    
+    while(1){
+        for(int i = 0; i<=9; i++){
+            PORTD = ~display[i];
+            __delay_ms(1000);
+        }
+    }
+}
