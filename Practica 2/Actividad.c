cba4a44f7431327c669d299c7e552ad4f0dd387e
Diego Castro <diego.castro@udem.edu>
2026-06-07 19:51:50 -0600

    Enhance comments for clarity in Actividad.c
    Added comments to clarify the purpose of loops and multiplexing.

--- Practica 2/Actividad.c
+++ Practica 2/Actividad.c
@@ -20,6 +20,7 @@
 
 #define _XTAL_FREQ 8000000      // Frecuencia del oscilador (para __delay_ms y __delay_us)
 
+// Declaración de patrones escritos en binario: 
 unsigned char patron_d[8] = {0b00111100, 0b01000011, 0b10000001, 0b10000001, 0b10000001, 0b10000001, 0b10000001, 0b11111111};
 unsigned char patron_g[8] = {0b10011111, 0b10010001, 0b10010001, 0b10010001, 0b10011001, 0b10000001, 0b10000001l, 0b11111111};
 unsigned char patron_m[8] = {0b11111111, 0b01000000, 0b00100000, 0b00010000, 0b00010000, 0b00100000, 0b01000000, 0b11111111};
@@ -35,18 +36,18 @@ void main(void){
     
     while(1){
         // LETRA D:
-        for(int t = 0; t < 25; t++){     // Retiene la imagen por ~1 segundo
+        for(int t = 0; t < 25; t++){     // El ciclo for de t retiene la imagen por aproximadamente un segundo, ya que 25*8*5 = 1000 ms. 
             for(char i = 0; i<8; i++){
-                PORTB = 1<<i; 
-                PORTD = ~patron_d[i]; 
+                PORTB = 1<<i; // Multiplexación. 
+                PORTD = ~patron_d[i]; // Display del patrón en la matriz de 8x8.   
                 __delay_ms(5);
             }   
         }  
-
+        // El procedimiento se repite para cada letra. 
         // LETRA G: 
         for(int t = 0; t < 25; t++){
            for(char i = 0; i<8; i++){
-               PORTB = 1<<i;
+               PORTB = 1<<i; // Multiplexación. 
                PORTD = ~patron_g[i]; 
                __delay_ms(5);
            }
@@ -55,7 +56,7 @@ void main(void){
         // LETRA M:
         for(int t = 0; t < 25; t++){
            for (char i=0; i<8; i++){
-                  PORTB = 1<<i;
+                  PORTB = 1<<i; // Multiplexación.
                   PORTD=~patron_m[i];
                   __delay_ms(5);
            }
@@ -64,7 +65,7 @@ void main(void){
         // LETRA N:
         for(int t = 0; t < 25; t++){
            for (char i=0;i<8;i++){
-                  PORTB = 1<<i;
+                  PORTB = 1<<i; // Multiplexación. 
                   PORTD=~patron_n[i];
                   __delay_ms(5);
            }
