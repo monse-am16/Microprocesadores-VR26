8c39992182691a1edcf427ee99ded3451ea17c03
Diego Castro <diego.castro@udem.edu>
2026-06-07 19:45:14 -0600

    Add main function and configuration for PIC microcontroller

---
+++ Practica 2/Clase.c
@@ -0,0 +1,41 @@
+#include <xc.h>         // Biblioteca principal del compilador XC8
+
+//=============================================================================
+// CONFIGURACI N DE BITS DE CONFIGURACI N (FUSES)
+//=============================================================================
+
+// Selecci n de oscilador (usar XT si est s usando un cristal de 4 MHz)
+#pragma config FOSC = HS        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (enabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (disabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection (disabled)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable (disabled)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (disabled)
+
+//=============================================================================
+// DEFINICIONES
+//=============================================================================
+
+#define _XTAL_FREQ 8000000      // Frecuencia del oscilador (para __delay_ms y __delay_us)
+
+unsigned char patron[8] = {0x81, 0x42, 0x24, 0x18, 0x18, 0x24, 0x42, 0x81};
+
+void main(void){
+    TRISB = 0; // Puerto B como salida (filas). 
+    TRISD = 0; // Puerto D como salida (columnas). 
+    
+    PORTB = 0; // Reset de puerto B.
+    PORTD = 0; // Reset de puerto D. 
+    
+    while(1){
+        for(char i = 0; i<8; i++){
+            PORTB = 1<<i; // Multiplexación. El 1 se recorre i espacios. 
+            PORTD = ~patron[i]; 
+            __delay_ms(5); 
+        }
+    }
+}
+
+
