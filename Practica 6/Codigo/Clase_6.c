7943b9bcf82438a8dc9b0dd4f46548401bd8a8f9
Diego Castro <diego.castro@udem.edu>
2026-06-14 10:16:41 -0600

    Update Clase_6.c

--- Practica 6/Codigo/Clase_6.c
+++ Practica 6/Codigo/Clase_6.c
@@ -1,1 +1,44 @@
+#include <xc.h>         // Biblioteca principal del compilador XC8
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "lcd.h"
 
+//=============================================================================
+// CONFIGURACI N DE BITS DE CONFIGURACI N (FUSES)
+//=============================================================================
+
+// Selecci n de oscilador (usar XT si est s usando un cristal de 4 MHz)
+#pragma config FOSC = HS        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (enabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (disabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection (disabled)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable (disabled)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (disabled)
+
+//=============================================================================
+// DEFINICIONES
+//=============================================================================
+
+#define _XTAL_FREQ 8000000      // Frecuencia del oscilador (para __delay_ms y __delay_us)
+
+void main(void){
+    // LCD solo puede ir a un puerto. 
+    LCD lcd = {&PORTC, 2, 3, 4, 5, 6, 7}; // PORT, RS, EN, D4, D5, D6 
+    LCD_Init(lcd); 
+    
+    while(1){
+        LCD_Clear(); 
+        LCD_Set_Cursor(0,0);
+        LCD_putrs(" HELLO WORLD! ");
+        LCD_Set_Cursor(1,0);
+        
+        for(char c = 'A'; c < 'Q'; c++){
+            LCD_putc(c);
+            __delay_ms(300);
+        }
+        __delay_ms(1000); 
+    }
+}
